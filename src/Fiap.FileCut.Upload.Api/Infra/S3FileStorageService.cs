78aa7c2779ac8f2f549b5c671559ec9868d51741
guilherme_novaes <guilherme.novaes233@gmail.com>
2025-02-03 22:15:41 -0300

    fix: chore dependencies

--- src/Fiap.FileCut.Upload.Api/Infra/S3FileStorageService.cs
+++ src/Fiap.FileCut.Upload.Api/Infra/S3FileStorageService.cs
@@ -22,7 +22,7 @@ namespace Fiap.FileCut.Upload.Api.Infra
 			{
 				_logger.LogInformation("[{source}] - Starting file deletion. User: {UserId}, File: {FileName}", nameof(FileService), userId, fileName);
 
-				var result = await _fileRepository.DeleteAsync(userId, fileName);
+				var result = await _fileRepository.DeleteAsync(userId, fileName, cancellationToken);
 
 				if (result)
 					_logger.LogInformation("[{source}] - File deleted successfully. User: {UserId}, File: {FileName}", nameof(FileService), userId, fileName);
@@ -44,7 +44,7 @@ namespace Fiap.FileCut.Upload.Api.Infra
 			{
 				_logger.LogInformation("[{source}] - Starting file download. User: {UserId}, File: {FileName}", nameof(FileService), userId, fileName);
 
-				return await _fileRepository.GetAsync(userId, fileName);
+				return await _fileRepository.GetAsync(userId, fileName, cancellationToken);
 			}
 			catch (Exception ex)
 			{
@@ -59,7 +59,7 @@ namespace Fiap.FileCut.Upload.Api.Infra
 			{
 				_logger.LogInformation("[{source}] - Starting file name listing. User: {UserId}", nameof(FileService), userId);
 
-				var files = await _fileRepository.GetAllAsync(userId);
+				var files = await _fileRepository.GetAllAsync(userId, cancellationToken);
 				var fileNames = files.Select(file => file.FileName).ToList();
 
 				_logger.LogInformation("[{source}] - Successfully listed {FileCount} file names for user {UserId}", nameof(FileService), fileNames.Count(), userId);
@@ -82,7 +82,7 @@ namespace Fiap.FileCut.Upload.Api.Infra
 
 				_logger.LogInformation("[{source}] - Starting file upload. User: {UserId}, File: {FileName}", nameof(FileService), userId, file.FileName);
 
-				var result = await _fileRepository.UpdateAsync(userId, file);
+				var result = await _fileRepository.UpdateAsync(userId, file, cancellationToken);
 				if (result)
 					_logger.LogInformation("[{source}] - File saved successfully. User: {UserId}, File: {FileName}", nameof(FileService), userId, file.FileName);
 				else
